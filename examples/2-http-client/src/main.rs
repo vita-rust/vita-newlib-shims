afd5a0c1a11717ab0f9909d062c96201ce877f9b
Nikolay Arhipov <n@arhipov.net>
2023-09-26 19:10:43 +0300

    Updated http client examples

--- examples/2-http-client/src/main.rs
+++ examples/2-http-client/src/main.rs
@@ -1,33 +1,36 @@
-use std::collections::HashMap;
+#[cfg(target_os = "vita")]
+use vita_newlib_shims as _;
 
-use oxhttp::{
-    model::{HeaderName, Method, Request, Status},
-    Client,
-};
-
-fn main() {
+fn main() -> anyhow::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "full");
 
     tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap()
+        .enable_all()
+        .build()?
         .block_on(async {
-            println!(">>> Entering async main in tokio");
-
-            let client = Client::new();
-            let response = client
+            println!(">>> Trying oxhttp");
+            let client = oxhttp::Client::new();
+            let body = client
                 .request(
-                    Request::builder(Method::GET, "http://example.com".parse().unwrap()).build(),
-                )
-                .unwrap();
-            println!(">>> oxhttp response status, {:?}", response.status());
-            assert_eq!(response.status(), Status::OK);
-            assert_eq!(
-                response.header(&HeaderName::CONTENT_TYPE).unwrap().as_ref(),
-                b"text/html; charset=UTF-8"
-            );
+                    oxhttp::model::Request::builder(
+                        oxhttp::model::Method::GET,
+                        "http://example.com".parse()?,
+                    )
+                    .build(),
+                )?
+                .into_body()
+                .to_string()?;
+            println!(">>> oxhttp response: {body}");
 
-            let body = response.into_body().to_string().unwrap();
-            println!(">>> oxhttp response body, {}", body);
-        });
+            // Requires std to depend on the latest libc
+            // println!(">>> Trying ureq");
+            // let body = ureq::get("http://example.com").call()?;
+            // println!(">>> Ureq response: {:?}", body);
+
+            println!(">>> Trying reqwest");
+            let body = reqwest::get("http://example.com").await?.text().await?;
+            println!(">>> Reqwest response: {:#?}", body);
+
+            Ok(())
+        })
 }
